d6861e607ffed853e8cd43fced1b459dd615bc42
MyKasero <mykasero20@gmail.com>
2024-09-07 02:39:39 +0200

    db errors with new models debugged succesfully

--- GLSR_Gym/Schedule/models.py
+++ GLSR_Gym/Schedule/models.py
@@ -1,25 +1,25 @@
 from django.db import models
-import datetime
+# import datetime
 # Create your models here.
-now = datetime.datetime.now()
+# now = datetime.datetime.now()
 class Booking(models.Model):
     id = models.AutoField(primary_key=True)
-    users = models.CharField(db_column="Uzytkownicy",max_length=100, default="User")
-    users_amount = models.IntegerField(db_column = "Ilosc osob", default=0)
+    users = models.CharField(db_column="Uzytkownicy",max_length=100)
+    users_amount = models.IntegerField(db_column = "Ilosc_osob")
     # start_hour + end_hour vs just start_hour TBD
-    start_hour = models.TimeField(db_column= "Start" , default = str(now.hour) + ":" + str(now.minute))
-    end_hour = models.TimeField(db_column = "Koniec", default = str(now.hour) + ":" + str(now.minute+1))
-    current_day = models.DateTimeField(db_column = "Data", default = str(now.day) + "/" + str(now.month) + "/" + str(now.year))
-    # pass
+    start_hour = models.TimeField(db_column= "Start")
+    end_hour = models.TimeField(db_column = "Koniec")
+    current_day = models.DateTimeField(db_column = "Data")
+#     # pass
     
 class Archive(models.Model):
-    #Usernames
-    #Hours + d/m/y auto added
+#     #Usernames
+#     #Hours + d/m/y auto added
     id = models.AutoField(primary_key=True)
-    users = models.CharField(db_column="Uzytkownicy",max_length=100, default="User")
-    users_amount = models.IntegerField(db_column = "Ilosc osob", default=0)
-    start_hour = models.TimeField(db_column= "Start" , default = str(now.hour) + ":" + str(now.minute))
-    end_hour = models.TimeField(db_column = "Koniec", default = str(now.hour) + ":" + str(now.minute+1))
-    current_day = models.DateTimeField(db_column = "Data", default = str(now.day) + "/" + str(now.month) + "/" + str(now.year))
+    users = models.CharField(db_column="Uzytkownicy",max_length=100)
+    users_amount = models.IntegerField(db_column = "Ilosc osob")
+    start_hour = models.TimeField(db_column= "Start")
+    end_hour = models.TimeField(db_column = "Koniec")
+    current_day = models.DateTimeField(db_column = "Data")
     
-    # pass
+#     # pass
