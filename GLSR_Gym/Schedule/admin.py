a4d6333713ede38bca92d1d9cfaf4783df780cd4
MyKasero <mykasero20@gmail.com>
2024-09-09 22:18:34 +0200

    Working on manual archiving first, registered models in admin

--- GLSR_Gym/Schedule/admin.py
+++ GLSR_Gym/Schedule/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from models import Booking, Archive
 # Register your models here.
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display=('id','users','users_amount','start_hour','end_hour','current_day')
+    search_fields=('users','current_day')
+    list_filter=('users','start_hour','current_day')
+
+@admin.register(Archive)
+class ArchiveAdmin(admin.ModelAdmin):
+    list_display=('id','users','users_amount','start_hour','end_hour','current_day')
+    search_fields=('users','current_day')
+    list_filter=('users','start_hour','current_day')     
