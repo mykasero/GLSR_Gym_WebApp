9c3bb19bcf6f033c75bb305d83a52fef77dd78b9
MyKasero <mykasero20@gmail.com>
2024-09-09 22:22:05 +0200

    created archiving bookings action for admin

--- GLSR_Gym/Schedule/admin.py
+++ GLSR_Gym/Schedule/admin.py
@@ -1,6 +1,30 @@
 from django.contrib import admin
 from models import Booking, Archive
+import logging
 # Register your models here.
+logger = logging.getLogger(__name__)
+
+@admin.action(description="Zarchiwizuj rezerwacje starsze niz dzisiejsze")
+def archive_bookings(modeladmin, request, queryset):
+    from django.utils import timezone
+    from django.db import transaction
+        
+    old_bookings = Booking.objects.filter(current_day__lt=timezone.now())
+    
+    if old_bookings.exists():
+        with transaction.atomic():
+            for booking in old_bookings:
+                Archive.objects.create(
+                    users = booking.users,
+                    users_amount = booking.users_amount,
+                    start_hour = booking.start_hour,
+                    end_hour = booking.end_hour,
+                    current_day = booking.current_day,
+                )
+                booking.delete()
+        logger.info("Success")
+    else:
+        logger.info("No rows to archive")
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
