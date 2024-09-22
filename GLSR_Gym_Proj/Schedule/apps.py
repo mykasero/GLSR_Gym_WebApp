a7a6b1621bc49ed9df235e088b0e896f75e24cd2
MyKasero <mykasero20@gmail.com>
2024-09-23 01:31:07 +0200

    changed name in apps / correction

--- GLSR_Gym_Proj/Schedule/apps.py
+++ GLSR_Gym_Proj/Schedule/apps.py
@@ -2,7 +2,7 @@ from django.apps import AppConfig
 
 class ScheduleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'GLSR_GYM_Proj.Schedule'
+    name = 'GLSR_Gym_Proj.Schedule'
     
     def ready(self):
         from . import jobs
