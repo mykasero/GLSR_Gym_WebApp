75efee8dfd4a6a52679ea2c186761a4a6a03b865
MyKasero <mykasero20@gmail.com>
2024-10-05 01:47:34 +0200

    added better helptext to booking

--- Schedule/models.py
+++ Schedule/models.py
@@ -7,7 +7,7 @@ class Booking(models.Model):
     id = models.AutoField(primary_key=True)
     users = models.CharField(db_column="Uzytkownicy",max_length=100, help_text="Podana nazwa musi być nazwą zarejestrowanego użytkownika")
     users_amount = models.IntegerField(db_column = "Ilosc_osob",help_text="Podaj jako numer")
-    start_hour = models.TimeField(db_column= "Start", help_text = "Przykładowy format: 12:20")
+    start_hour = models.TimeField(db_column= "Start", help_text = "Podaj godzine w formacie np.: 12:20, lub zacznij podawać numery i wybierz odpowiadający ci z listy domyślnych wyborów (na tel nad klawiaturą)")
     end_hour = models.TimeField(db_column = "Koniec", help_text = "Ta godzina moze byc 'na oko', format taki sam jak powyżej")
     current_day = models.DateField(db_column = "Data", help_text = "Wybierz dzień" )
     
@@ -18,6 +18,8 @@ class Archive(models.Model):
     start_hour = models.TimeField(db_column= "Start")
     end_hour = models.TimeField(db_column = "Koniec")
     current_day = models.DateField(db_column = "Data")
+    class Meta:
+        ordering = ["-current_day"]
     
 
 class Keycodes(models.Model):
