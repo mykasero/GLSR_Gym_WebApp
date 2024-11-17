0d54dc85e75ad2fc7e19082e0f6895a143e89f0f
MyKasero <mykasero20@gmail.com>
2024-11-17 19:02:27 +0100

    small tweaks for docker console output when testing

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,5 +3,5 @@ bind = "0.0.0.0:8000"
 chdir = "/app/"
 module = "app.wsgi:application"
 
-# certfile = "/etc/letsencrypt/live/glsr.com/fullchain.pem"
-# keyfile = "etc/letsencrypt/live/glsr.com/privkey.pem"
+accesslog = "-"
+errorlog = "-"
