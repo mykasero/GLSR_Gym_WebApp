a013cb6d2268809da15434c7fb69bf3ba7bf8eb4
MyKasero <mykasero20@gmail.com>
2024-12-26 18:12:42 +0100

    test

--- GLSR_Gym/settings.py
+++ GLSR_Gym/settings.py
@@ -96,11 +96,17 @@ STORAGES = {
     # Media file (image) management
     "default" : {
         # "BACKEND" : "storages.backends.s3boto3.S3StaticStorage",
-        "BACKEND" : "storages.settings.aws.storage_backends.MediaStorage",
+        "BACKEND" : "storages.backends.s3boto3.S3Boto3Storage",
+        "OPTIONS" : {
+            "location" : "media",
+        }
     },
     # Static file management
     "staticfiles" : {
         "BACKEND" : "storages.backends.s3boto3.S3StaticStorage",
+        "OPTIONS" : {
+            "location" : "static",
+        }
     },
 }
 
@@ -108,6 +114,7 @@ AWS_ACCESS_KEY_ID =  env("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
 AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
 AWS_QUERYSTRING_AUTH = env("AWS_QUERYSTRING_AUTH")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
 AWS_DEFAULT_ACL='public-read'
 AWS_S3_FILE_OVERWRITE = False
 
