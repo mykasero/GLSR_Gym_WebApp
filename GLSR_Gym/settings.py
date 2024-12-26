e7afe635ab6be072ac94fec9cc0603d303a24beb
MyKasero <mykasero20@gmail.com>
2024-12-26 03:47:02 +0100

    test3

--- GLSR_Gym/settings.py
+++ GLSR_Gym/settings.py
@@ -216,7 +216,8 @@ REST_FRAMEWORK = {
 from corsheaders.defaults import default_methods, default_headers
 CORS_ALLOW_ALL_ORIGINS = False
 if DEBUG:
-    CORS_ALLOWED_ORIGINS = json.loads(env('CORS_ALLOWED_ORIGINS'))
+    # CORS_ALLOWED_ORIGINS = json.loads(env('CORS_ALLOWED_ORIGINS'))
+    pass
 else:
     CORS_ALLOWED_ORIGINS = (env("CORS_ALLOWED_ORIGINS_PROD"))
 CORS_ALLOW_METHODS = (
