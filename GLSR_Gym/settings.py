d88cc62b7dda59ab02d530c8bda5808e496203a1
MyKasero <mykasero20@gmail.com>
2024-12-26 18:31:43 +0100

    test

--- GLSR_Gym/settings.py
+++ GLSR_Gym/settings.py
@@ -270,17 +270,12 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'file': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': 'django_error.log',
+        'console': {
+            'class': 'logging.StreamHandler',
         },
     },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
     },
 }
