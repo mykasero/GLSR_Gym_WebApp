bec9604042b6529771081a3727944c59c29e9502
MyKasero <mykasero20@gmail.com>
2025-02-04 22:43:42 +0100

    added Payments model into the adminPanel

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from .models import Profile
+from .models import Profile, Payment
 from Schedule.admin import admin_site
 
 # profiles list shown in admin
@@ -8,7 +8,16 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ["user","email","date_joined"]
     fields = ["user","email","date_joined","profile_picture"]
 
+# payments list shown in admin
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ["user","is_paid","payment_date","expiry_date"]
+    field = ["user","is_paid","payment_date","expiry_date"]
+
 # register profiles if they don't exist already in the admin profiles list
 if Profile not in admin_site._registry:
     admin_site.register(Profile, ProfileAdmin)
     
+# register payments if they don't exist already in the admin payments list
+    
+if Payment not in admin_site._registry:
+    admin_site.register(Payment, PaymentAdmin)
