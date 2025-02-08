92df1ce8c9edf7c0751fcc242b83ab7e2bf7e625
MyKasero <mykasero20@gmail.com>
2025-02-08 19:30:22 +0100

    created a decorator for checking if user is_active is False

---
+++ Schedule/decorators.py
@@ -0,0 +1,20 @@
+from functools import wraps
+from django.shortcuts import redirect
+from django.contrib import messages
+
+def user_is_active(redirect_url=None):
+    '''
+    Decorator for checking if the currently logged in user has is_active=True
+    '''
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapped_view(request, *args, **kwargs):
+            if not request.user.is_active:
+                messages.error(
+                    request, 
+                    "Nie można wyświelić żądanej strony, ponieważ konto jest deaktywowane." +
+                    " Aby aktywować konto, należy opłacić składke.")
+                return redirect(redirect_url)
+            return view_func(request, *args, **kwargs)
+        return _wrapped_view
+    return decorator
